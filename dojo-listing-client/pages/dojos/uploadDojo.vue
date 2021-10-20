35a51c5523810e96b8bd7cc978b2568599e77dcd
Cal <marsc0388@gmail.com>
2021-10-20 12:48:18 +0900

    Feat: Refactor profile page

--- dojo-listing-client/pages/dojos/uploadDojo.vue
+++ dojo-listing-client/pages/dojos/uploadDojo.vue
@@ -1,22 +1,21 @@
 <template>
     <div>
+      <b-container>
         <b-form @submit.prevent="upload">
             <b-form-group
-      id="titleset"
-      description="Let us know the dojo title."
-      label="Enter the dojo title"
-      label-for="title"
-    >
-      <b-form-input id="title" v-model="title" trim></b-form-input>
-    </b-form-group>
-    <b-form-group
-      id="kanchouset"
-      description="Let us know the dojo leader."
-      label="Who is the kanchou?"
-      label-for="kanchou"
-    >
-      <b-form-input id="kanchou" v-model="kanchou" trim></b-form-input>
-    </b-form-group>
+            id="titleset"
+            description="Let us know the dojo title."
+            label="Enter the dojo title"
+            label-for="title">
+              <b-form-input id="title" v-model="title" trim></b-form-input>
+            </b-form-group>
+            <b-form-group
+            id="kanchouset"
+            description="Let us know the dojo leader."
+            label="Who is the kanchou?"
+            label-for="kanchou">
+              <b-form-input id="kanchou" v-model="kanchou" trim></b-form-input>
+            </b-form-group>
             <b-form-file
             v-model="file1"
             :state="Boolean(file1)"
@@ -26,10 +25,12 @@
             <div class="mt-3">Selected file: {{ file1 ? file1.name : ''}}</div>
             <b-button type="submit" variant="primary">Submit</b-button>
         </b-form>
+      </b-container>
     </div>
 </template>
 <script>
 export default {
+    middleware: 'auth',
     data() {
         return {
             title: '',
@@ -38,6 +39,7 @@ export default {
         }
     },
     methods: {
+      
         async upload() {
           try {
             const dojo = await this.$axios.$post('/api/dojos', {
@@ -51,6 +53,9 @@ export default {
           }
           catch(error) {
             console.log(error);
+          }
+          finally {
+            console.log(file1);
           }
         }
     }
--- dojo-listing-client/pages/user/my-profile.vue
+++ dojo-listing-client/pages/user/my-profile.vue
@@ -1,41 +1,30 @@
 <template>
     <div>
-        <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
-    <template #header>Hi {{this.$auth.user.full_name}}</template>
-
-    <template #lead>
-      {{this.$auth.user.email}}
-    </template>
-
-    <hr class="my-4">
-
-    <p>
-      It uses utility classes for typography and spacing to space content out within the larger
-      container.
-    </p>
-  </b-jumbotron>
+        <b-container>
         <b-card
-        title="Profile"
+        title="Your profile"
         img-src="https://picsum.photos/600/300/?image=25"
         img-alt="Image"
         img-top
         tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
+        style="max-width: 50%;"
+        class="mt-3 text-center"
     >
     <b-card-body>
-        <h4></h4>
-        <h4></h4>
-        <nuxt-link to="/user/logout">Logout</nuxt-link>
+        <h4>{{this.$auth.user.full_name}}</h4>
+        
     </b-card-body>
         <b-card-text>
+            {{this.$auth.user.email}}
         </b-card-text>
+        <nuxt-link to="/user/logout">Logout</nuxt-link>
     </b-card>
+    </b-container>
     </div>
 </template>
 
 <script>
 export default {
-    middleware: 'auth'
+    middleware: 'auth',
 }
 </script>
