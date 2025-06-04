779d62d8dfbc3cda82352065dcd900dfd29f8686
marsc0388 <marsc0388@gmail.com>
2025-06-04 18:56:41 +0900

    feat: tailwind and shadcn vue with card and navigation menu components

--- dojo-listing-client/nuxt.config.ts
+++ dojo-listing-client/nuxt.config.ts
@@ -1,9 +1,26 @@
-import { defineNuxtConfig } from 'nuxt/config';
+import { defineNuxtConfig } from "nuxt/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  devtools: { enabled: true },
   // This is for enabling type checking in development or build time
   typescript: {
-    typeCheck: true
+    typeCheck: true,
   },
-  modules: ['@pinia/nuxt']
-})
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ["@pinia/nuxt", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
