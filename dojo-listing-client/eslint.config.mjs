7fda67454593d9df42de97244d89ac270315f823
marsc0388 <marsc0388@gmail.com>
2025-06-18 23:38:10 +0900

    feat: allow eslint to format on save and remove prettier

--- dojo-listing-client/eslint.config.mjs
+++ dojo-listing-client/eslint.config.mjs
@@ -1,27 +1,26 @@
 import antfu from '@antfu/eslint-config'
-// import pluginVue from "eslint-plugin-vue";
-import prettier from 'eslint-plugin-prettier'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   {
-    ignores: ['.config/', 'dist/', 'tsconfig.json', './apiOld/'], // acts as global ignores, due to the absence of other properties
+    ignores: [
+      '.config/',
+      'dist/',
+      'tsconfig.json',
+      './apiOld/',
+      './components/ui/',
+    ], // acts as global ignores, due to the absence of other properties
   },
   antfu({
-    // ...@antfu/eslint-config options
+    stylistic: {
+      semi: false,
+      singleQuote: true,
+    },
   }),
-  // Prettier config
   {
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
-    },
-    plugins: {
-      prettier,
-    },
-    rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     },
     linterOptions: {
       reportUnusedDisableDirectives: true,
